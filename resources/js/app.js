7c12d007fd7dfd2ebf9ae61d56b4b8c600e5af4e
silverhand <refojunior5@gmail.com>
2022-12-17 10:46:47 +0800

    install vue to frontend project with calendar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+// app.js
 import './bootstrap';
+
+import {createApp} from 'vue'
+
+import CalendarApp from './pages/CalendarPage.vue'
+
+createApp(CalendarApp).mount("#calendar-app");
---
+++ resources/js/pages/CalendarPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <TheCalendar :bookings="bookings" />
+    </div>
+</template>
+
+<script>
+import TheCalendar from '../components/TheCalendar.vue';
+export default {
+  data() {
+    return {
+      bookings: []
+    }
+  },
+  components: {
+    TheCalendar,
+  },
+}
+</script>
